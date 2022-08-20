3761614b1c2e4437914fd2c1fe327102f6b48a49
sarrim <sarmad.iict@gmail.com>
2022-08-20 18:36:25 +0500

    tasks

---
+++ task-2.py
@@ -0,0 +1,16 @@
+import sys
+print(sys.version)
+
+# print(sys.version_info)
+
+# print(sys.argv)
+
+# str = 'Twinkle, twinkle, little star'
+# count = 0;
+
+# for i in str:
+#     if(str.count(i)):
+#         print(i)
+#         count = count+1
+    
+# print('number of characters not same are', count)
